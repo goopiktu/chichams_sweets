bd54335b381d03e82525fac59a1b9d64476f7e49
Aldwin <91401877+goopiktu@users.noreply.github.com>
2023-10-13 06:09:55 +0800

    Add: Form

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,12 +1,22 @@
 import Homepage from './components/homepage/homepage.jsx'
+import CustomerForm from './components/form/form.jsx'
 import 'bootstrap/dist/css/bootstrap.min.css';
+
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 function App() {
   return (
-    <div className="App">
-      <Homepage />
+    <BrowserRouter> 
+      <Routes>
+        <Route path='/' element={<Homepage/>} />
+        <Route path='/Form' element={<CustomerForm/>} />
+      </Routes>
+    
+    </BrowserRouter>
 
-    </div>
+
+    
   );
 }
 
