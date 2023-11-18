a5e96a8e7a100e7f0eca2f50503d3ef3fa41ae14
skeeDayo <steven_escopete@dlsu.edu.ph>
2023-11-18 17:29:31 +0800

    refactor contact us page

--- frontend/src/components/contact_us/contactus.jsx
+++ frontend/src/components/contact_us/contactus.jsx
@@ -1,5 +1,6 @@
 import './contactus.css';
 import NavBar from '../navbar/navbar.jsx';
+import Footer from '../footer/footer.jsx';
 
 import MapPin from './assets/map-pin.png';
 import CellPhone from './assets/cellphone.png';
@@ -9,131 +10,82 @@ import { useState, useEffect } from 'react';
 import { useNavigate } from 'react-router-dom';
 
 function ContactUs() {
-        const [destLink, setDestLink] = useState('/contactus');
+  const [destLink, setDestLink] = useState('/contactus');
 
-        const getDestLink = (event, destString) => {
-                setDestLink('/'.concat(destString));
-        }
+  const getDestLink = (event, destString) => {
+    setDestLink('/'.concat(destString));
+  };
 
-        const navigate = useNavigate();
+  const navigate = useNavigate();
 
-        const handleNavigation = () => {
-                navigate(destLink);
-        }
+  const handleNavigation = () => {
+    navigate(destLink);
+  };
 
-        const ping = (event, dest) => {
-                console.log(event);
-                console.log("you clicked: " + dest);
-        }
+  const ping = (event, dest) => {
+    console.log(event);
+    console.log('you clicked: ' + dest);
+  };
 
-        useEffect(() => {
-                // getDestLink();
-                console.log((destLink))
-                handleNavigation();
-        }, [destLink])
+  useEffect(() => {
+    // getDestLink();
+    console.log(destLink);
+    handleNavigation();
+  }, [destLink]);
 
-        return(
-                <div className="contactus-div">
-                        <NavBar/>
-                        <div className="header-text">
-                                Contact Us
-                        </div>
+  return (
+    <>
+      <NavBar />
 
-                        <div className="about-us-divider">
+      <section className="header">Contact Us</section>
+      <div className="header-divider"></div>
 
-                        </div>
+      <section className="contact-us-body">
+        <section className="contact-us-item">
+          <p className="contact-us-item-name">Jeannie Ortilla-Tolentino</p>
+          <div className="contact-us-divider"></div>
 
-                        <div className="contact-us-contact-info">
-                                <div className="contact-us-owner-info-div">
-                                        <div className="contact-us-owner-info">
-                                                <div className="contact-us-owner-name">
-                                                        Jeannie Ortilla-Tolentino
-                                                </div>
-                                                <div className="contact-us-line-divider">
+          <section className="contact-us-item-info">
+            <div className="contact-us-item-detail">
+              <img className="contact-us-icon" alt="" src={MapPin} />
+              <a
+                className="redirect-link"
+                target="_blank"
+                href="https://www.google.com/maps/place/Chicham's+Sweet+Delight+Cakes+and+Pastries+Shop/@14.3873924,120.9690956,17z/data=!3m1!4b1!4m6!3m5!1s0x3397d38788bc9a23:0xe055952009b1ca36!8m2!3d14.3873873!4d120.9739665!16s%2Fg%2F11mgnbmm8v?entry=ttu"
+              >
+                Molino 4, Bacoor, Cavite
+              </a>
+            </div>
+            <div className="contact-us-item-detail">
+              <img className="contact-us-icon" alt="" src={CellPhone} />
+              <span className="text">0917 825 2905</span>
+            </div>
+            <div className="contact-us-item-detail">
+              <img className="contact-us-icon" alt="" src={FaceBook} />
+              <a
+                target="_blank"
+                className="redirect-link"
+                href="https://www.facebook.com/Chichamsweetdelight"
+              >
+                https://www.facebook.com/Chichamsweetdelight
+              </a>
+            </div>
+            <div className="contact-us-item-detail">
+              <img className="contact-us-icon" alt="" src={Email} />
+              <a
+                href="mailto:jeannie_tolentino@yahoo.com"
+                className="redirect-link"
+              >
+                jeannie_tolentino@yahoo.com
+              </a>
+            </div>
+          </section>
+        </section>
+      </section>
 
-                                                </div>
-                                                <div className="contact-us-contact-info">
-                                                        <div className="contact-us-contact-data">
-                                                                <div className="contact-us-social-icon">
-                                                                        <img
-                                                                                className="contact-us-social-icon-img"
-                                                                                src={MapPin}
-                                                                        />
-                                                                </div>
-
-                                                                <div className="contact-us-social-details">
-                                                                                <a className="redirect-link" target="_blank" href="https://www.google.com/maps/place/Chicham's+Sweet+Delight+Cakes+and+Pastries+Shop/@14.3873924,120.9690956,17z/data=!3m1!4b1!4m6!3m5!1s0x3397d38788bc9a23:0xe055952009b1ca36!8m2!3d14.3873873!4d120.9739665!16s%2Fg%2F11mgnbmm8v?entry=ttu">
-                                                                                        Molino 4, Bacoor, Cavite
-                                                                                </a>
-                                                                </div>
-                                                        </div>
-
-                                                        <div className="contact-us-contact-data">
-                                                                <div className="contact-us-social-icon">
-                                                                        <img
-                                                                                className="contact-us-social-icon-img"
-                                                                                src={CellPhone}
-                                                                        />
-                                                                </div>
-
-                                                                <div className="contact-us-social-details">
-                                                                                0917 825 2905
-                                                                </div>
-                                                        </div>
-
-                                                        <div className="contact-us-contact-data">
-                                                                <div className="contact-us-social-icon">
-                                                                        <img
-                                                                                className="contact-us-social-icon-img"
-                                                                                src={FaceBook}
-                                                                        />
-                                                                </div>
-
-                                                                <div className="contact-us-social-details">
-                                                                        <a target="_blank" className="redirect-link" href='https://www.facebook.com/Chichamsweetdelight'>https://www.facebook.com/Chichamsweetdelight</a>
-                                                                </div>
-                                                        </div>
-
-                                                        <div className="contact-us-contact-data">
-                                                                <div className="contact-us-social-icon">
-                                                                        <img
-                                                                                className="contact-us-social-icon-img"
-                                                                                src={Email}
-                                                                        />
-                                                                </div>
-
-                                                                <div className="contact-us-social-details">
-                                                                        <a href="mailto:jeannie_tolentino@yahoo.com" className="redirect-link">jeannie_tolentino@yahoo.com</a>
-                                                                </div>
-                                                        </div>
-                                                </div>
-                                        </div>
-                                </div>
-                        </div>
-
-                        <div className="contact-us-nav-footer">
-                                <div className="contact-us-nav-footer-text">
-                                        <div className="contact-us-nav-footer-company-name" onClick={event => getDestLink(event, "")}>
-                                                Chicham's Sweet Delight Cakes and Pastries
-                                        </div>
-
-                                        <div className="contact-us-nav-footer-nav-options">
-                                                <div className="contact-us-options" onClick={event => getDestLink(event, "contactus")}>
-                                                        Contact Us
-                                                </div>
-
-                                                <div className="contact-us-options" onClick={event => getDestLink(event, "aboutus")}>
-                                                        About Us
-                                                </div>
-
-                                                <div className="contact-us-options" onClick={event => getDestLink(event, "products")}>
-                                                        Products
-                                                </div>
-                                        </div>
-                                </div>
-                        </div>
-                </div>
-        );
+      <Footer />
+    </>
+  );
 }
 
 export default ContactUs;
